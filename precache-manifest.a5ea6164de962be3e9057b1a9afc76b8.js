f09b7cb107fa9ddecafe2a433d73f45ce93cd05f
udakshataw <akshata.waghe@udgama.com>
2019-11-28 13:14:30 +0530

    Updates

---
+++ precache-manifest.a5ea6164de962be3e9057b1a9afc76b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd478867a6665f088a31b9383cacb3d",
+    "url": "/conFusion/index.html"
+  },
+  {
+    "revision": "a95ae9a17be316a53839",
+    "url": "/conFusion/static/css/2.21b67587.chunk.css"
+  },
+  {
+    "revision": "9e6c709c6b7d3245bac8",
+    "url": "/conFusion/static/css/main.42a102a2.chunk.css"
+  },
+  {
+    "revision": "a95ae9a17be316a53839",
+    "url": "/conFusion/static/js/2.c592a58f.chunk.js"
+  },
+  {
+    "revision": "9e6c709c6b7d3245bac8",
+    "url": "/conFusion/static/js/main.006d05a9.chunk.js"
+  },
+  {
+    "revision": "d75f40fedbbda6a7d2ca",
+    "url": "/conFusion/static/js/runtime~main.cac4731c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/conFusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/conFusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/conFusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/conFusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/conFusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
